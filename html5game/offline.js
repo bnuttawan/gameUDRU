aeb58bacd428c372963c312f27ccf0c3681630b5
bnuttawan <bownus@gmail.com>
2024-12-20 14:29:07 +0700

    upload print game

---
+++ html5game/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1734679480,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bgstart-sheet0.png",
+		"images/btplay-sheet0.png",
+		"images/bggame-sheet0.png",
+		"images/pic-sheet0.png",
+		"images/particles.png",
+		"images/wrong-sheet0.png",
+		"images/bgfinish-sheet0.png",
+		"images/bthome-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
